8272f8996e56d6136c76922d748507a9216c74eb
PieroMS <piero.mendieta6@gmail.com>
2025-04-04 19:16:42 -0500

    relaciones de modelos y migraciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,35 +9,18 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -45,4 +28,47 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // Relaciones
+
+    public function rols()
+    {
+        return $this->belongsTo(Rol::class, 'rol_id');
+    }
+
+    public function permissions()
+    {
+        return $this->hasMany(Permission::class, 'user_id'); //No es necesario declarar a user_id
+    }
+
+    public function extrahours()
+    {
+        return $this->hasMany(Extrahour::class, 'user_id');
+    }
+
+    public function assists()
+    {
+        return $this->hasMany(Assist::class, 'user_id');
+    }
+
+    public function companies()
+    {
+        return $this->belongsTo(Companie::class, 'companie_id');
+    }
+
+    public function jobs()
+    {
+        return $this->belongsTo(Job::class, 'job_id');
+    }
+
+    public function departments()
+    {
+        return $this->belongsTo(Department::class, 'department_id');
+    }
+
+    public function schedules()
+    {
+        return $this->belongsTo(Schedule::class, 'schedule_id');
+    }
+
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',50);
+            $table->string('lastname',50);
+            $table->string('document_type',10);
+            $table->string('document_number',11);
+            $table->datetimes('employment_date');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone',15);
+            $table->foreignId('rol_id')->constrained('rols')->onDelete('restrict');
+            $table->foreignId('companie_id')->constrained('companies')->onDelete('restrict');
+            $table->foreignId('department_id')->constrained('departments')->onDelete('restrict');
+            $table->foreignId('job_id')->constrained('jobs')->onDelete('restrict');
+            $table->foreignId('schedule_id')->constrained('schedules')->onDelete('restrict');
+            $table->string('status',1);
+            $table->string('qr_code',255);
             $table->rememberToken();
             $table->timestamps();
         });
